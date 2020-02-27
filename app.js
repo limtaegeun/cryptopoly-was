5310ffcf57ed4ad3848e3a4efdcfb789cd7f6bab
taegeun lim <imori333@gmail.com>
2020-02-27 23:27:24 +0900

    init

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require("body-parser");
+var cors = require("cors");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var routes = require("./routes");
+var app = express();
+var compression = require("compression");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(compression());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  cors({
+    origin: [
+      /\localhost:[0-9]+$/,
+      /\.cryptoply\.com$/,
+      /cryptoply\.com$/,
+    ]
+  })
+);
+
+console.log("port: ", process.env.PORT, "mode: ", process.env.NODE_ENV);
+
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
